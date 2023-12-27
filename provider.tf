16a5d510a339bc0f3f166b32241f019bf723108b
Kapil Kc <119867742+codewithkapil1@users.noreply.github.com>
2023-12-27 20:25:17 +0545

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.10.0"
+      version = "5.31.0"
+      
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
+  access_key = "AKIAS2BADFVDF632FL4U"
+  secret_key = "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"
+  
 }
